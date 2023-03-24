ac3c1ca2c57c8475a2f1db48143d0de01c55cfdb
781calmstate <frontenddevjscript@gmail.com>
2023-03-24 16:29:38 +0200

    added styles to navbar

--- src/UI/navbar/Navbar.jsx
+++ src/UI/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { AuthContext } from '../../context';
 import MyButton from '../button/MyButton';
+import cl from './Navbar.module.css';
 
 const Navbar = () => {
   const { isAuth, setIsAuth } = useContext(AuthContext);
@@ -11,20 +12,22 @@ const Navbar = () => {
     localStorage.removeItem('auth');
   };
   return (
-    <div>
-      <nav className="navbar">
-        <MyButton onClick={logout}>Sign out</MyButton>
-        <ul className="navbar__links">
-          <li>
+    <div style={{ width: '100%' }}>
+      <nav className={cl.navbar}>
+        <ul className={cl.menu}>
+          <li className={cl.link}>
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className={cl.link}>
             <Link to="/posts">To posts</Link>
           </li>
-          <li>
+          <li className={cl.link}>
             <Link to="/contact">Contact</Link>
           </li>
         </ul>
+        <button className={cl.button} onClick={logout}>
+          Sign out
+        </button>
       </nav>
       <Outlet />
     </div>
