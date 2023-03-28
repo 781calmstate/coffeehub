b437a6c3a65db336f071d9afd1ed550ebed635ed
781calmstate <frontenddevjscript@gmail.com>
2023-03-28 14:41:40 +0300

    Updates

---
+++ precache-manifest.30641c25300f2cd62c00a44d0da9ed07.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddaabfa7aa6bdc6355f7",
+    "url": "/coffeehub-react/static/css/main.1158b73d.chunk.css"
+  },
+  {
+    "revision": "ddaabfa7aa6bdc6355f7",
+    "url": "/coffeehub-react/static/js/main.1fae12c0.chunk.js"
+  },
+  {
+    "revision": "58d3ce24f11f28d09622",
+    "url": "/coffeehub-react/static/js/runtime~main.579760ad.js"
+  },
+  {
+    "revision": "f4c24abe251135c7f104",
+    "url": "/coffeehub-react/static/js/2.9bde637d.chunk.js"
+  },
+  {
+    "revision": "1a358a243f717a8c3b7ecafac3e8d586",
+    "url": "/coffeehub-react/static/media/coffee_logo.1a358a24.jpg"
+  },
+  {
+    "revision": "981d467f9c4f54280d80ae4aa256194d",
+    "url": "/coffeehub-react/static/media/coffee_phone.981d467f.png"
+  },
+  {
+    "revision": "13a238cf2b82ebadf4b09d1ed7e8a7a3",
+    "url": "/coffeehub-react/static/media/email.13a238cf.svg"
+  },
+  {
+    "revision": "d8cc9e46ab6bd3ed6e549fe17e21cf95",
+    "url": "/coffeehub-react/static/media/chat.d8cc9e46.svg"
+  },
+  {
+    "revision": "73f9a0ede528882146ea4bfe2ae935b5",
+    "url": "/coffeehub-react/static/media/Telegram_logo.73f9a0ed.svg"
+  },
+  {
+    "revision": "0b6b3c8d2c74fc2e0be8f5d940ec1e14",
+    "url": "/coffeehub-react/static/media/instagram.0b6b3c8d.png"
+  },
+  {
+    "revision": "147f41c95f6ad7574168c3d3a462c455",
+    "url": "/coffeehub-react/static/media/discord.147f41c9.svg"
+  },
+  {
+    "revision": "9faa97d8f9cf45e5fd2aa2c5b88f56af",
+    "url": "/coffeehub-react/static/media/morning_coffee.9faa97d8.jpg"
+  },
+  {
+    "revision": "d702c35d416d20fb31707d49feed8535",
+    "url": "/coffeehub-react/static/media/coffee_background.d702c35d.jpg"
+  },
+  {
+    "revision": "80d2bf6fd2f6c9eec14d55f57064a71e",
+    "url": "/coffeehub-react/index.html"
+  }
+];
